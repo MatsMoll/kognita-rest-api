8d1d27c737388f861830d74e6ad26383664bee13
Mats Eikeland Mollestad <mem@mollestad.no>
2019-12-18 15:46:05 +0100

    Removed autogenerated swift test

--- Tests/KognitaAPITests/KognitaAPITests.swift
+++ Tests/KognitaAPITests/KognitaAPITests.swift
@@ -2,14 +2,8 @@ import XCTest
 @testable import KognitaAPI
 
 final class KognitaAPITests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(KognitaAPI().text, "Hello, World!")
-    }
 
     static var allTests = [
-        ("testExample", testExample),
+//        ("testExample", testExample),
     ]
 }
