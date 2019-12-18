d81ac24f3bc91e9af13a609bc86237f7789c5058
Mats Eikeland Mollestad <mem@mollestad.no>
2019-12-18 15:47:52 +0100

    Fixed test compile error

--- Tests/KognitaAPITests/KognitaAPITests.swift
+++ Tests/KognitaAPITests/KognitaAPITests.swift
@@ -2,8 +2,14 @@ import XCTest
 @testable import KognitaAPI
 
 final class KognitaAPITests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+
+    }
 
     static var allTests = [
-//        ("testExample", testExample),
+        ("testExample", testExample),
     ]
 }
