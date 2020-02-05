63d09d9aca6588aab916511145d03e87c9095a3f
Mats Eikeland Mollestad <mem@mollestad.no>
2020-02-05 13:46:38 +0100

    [fix][HTTPS] Updated https middlaweare

--- Sources/KognitaAPI/KognitaAPI.swift
+++ Sources/KognitaAPI/KognitaAPI.swift
@@ -193,10 +193,29 @@ public class KognitaAPI {
 
 class HTTPSRedirectMiddleware: Middleware {
     func respond(to request: Request, chainingTo next: Responder) throws -> EventLoopFuture<Response> {
-        guard request.http.headers.firstValue(name: HTTPHeaderName("X-Forwarded-Proto")) == "http" else {
+        guard request.environment == Environment.production else {
             return try next.respond(to: request)
         }
-        let redirectPath = request.http.urlString.replacingOccurrences(of: "http://", with: "https://")
-        return request.future(request.redirect(to: redirectPath, type: RedirectType.temporary))
+
+        let proto = request.http.headers.firstValue(name: HTTPHeaderName("X-Forwarded-Proto"))
+            ?? request.http.url.scheme
+            ?? "http"
+
+        guard proto == "https" else {
+            guard let host = request.http.headers.firstValue(name: .host) else {
+                throw Abort(.badRequest)
+            }
+
+            let httpsURL = "https://" + host + request.http.urlString
+            return request.future(request.redirect(to: httpsURL, type: .permanent))
+        }
+
+        return try next.respond(to: request)
+            .map { resp in
+                resp.http.headers.add(
+                    name: "Strict-Transport-Security",
+                    value: "max-age=31536000; includeSubDomains; preload")
+                return resp
+            }
     }
 }
