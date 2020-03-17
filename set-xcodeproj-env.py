7668d638ee05eeadd3603b4dded829eec73dda0b
eskildbr <eskildbr@stud.ntnu.no>
2020-03-17 15:24:55 +0100

    [Test] Added API tests for task discussions and responeses

--- set-xcodeproj-env.py
+++ set-xcodeproj-env.py
@@ -5,15 +5,19 @@ file = "KognitaAPI.xcodeproj/xcshareddata/xcschemes/KognitaAPI-Package.xcscheme"
 
 tree = ETree.parse(file)
 
-env_var_section = ETree.SubElement(tree.getroot().find("LaunchAction"), "EnvironmentVariables")
-mailgun_key = ETree.SubElement(env_var_section, "EnvironmentVariable")
-mailgun_key.attrib["key"] = "MAILGUN_KEY"
-mailgun_key.attrib["value"] = "dd"
-mailgun_key.attrib["isEnabled"] = "YES"
-mailgun_domain = ETree.SubElement(env_var_section, "EnvironmentVariable")
-mailgun_domain.attrib["key"] = "MAILGUN_DOMAIN"
-mailgun_domain.attrib["value"] = "dd"
-mailgun_domain.attrib["isEnabled"] = "YES"
+launch_action = ETree.SubElement(tree.getroot(), "LaunchAction")
+launch_action.set("buildConfiguration", "Debug")
+launch_action.set("selectedDebuggerIdentifier", "Xcode.DebuggerFoundation.Debugger.LLDB")
+launch_action.set("selectedLauncherIdentifier", "Xcode.DebuggerFoundation.Launcher.LLDB")
+launch_action.set("launchStyle", "0")
+launch_action.set("useCustomWorkingDirectory", "NO")
+launch_action.set("ignoresPersistentStateOnLaunch", "NO")
+launch_action.set("debugDocumentVersioning", "YES")
+launch_action.set("debugServiceExtension", "internal")
+launch_action.set("allowLocationSimulation", "YES")
+
+env_var_section = ETree.SubElement(launch_action, "EnvironmentVariables")
+
 database_user = ETree.SubElement(env_var_section, "EnvironmentVariable")
 database_user.attrib["key"] = "DATABASE_USER"
 database_user.attrib["value"] = getpass.getuser()
