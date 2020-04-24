2f7ca1c70b9e33fde3c9e2ac71eec63c864233df
Mats Eikeland Mollestad <mem@mollestad.no>
2020-04-24 11:01:03 +0200

    [fix] Internal error when not completing any tasks

--- Tests/KognitaAPITests/PracticeSessionTests.swift
+++ Tests/KognitaAPITests/PracticeSessionTests.swift
@@ -11,12 +11,20 @@ import KognitaCoreTestable
 
 final class PracticeSessionTests: VaporTestCase {
     
-    
-    func testToAssignTask() throws {
+    func testResultsWithNoAnswers() {
+        failableTest {
+            let user = try User.create(on: conn)
+            let task = try Task.create(on: conn)
+            let session = try PracticeSession.create(in: [task.subtopicID], for: user, on: conn)
+            _ = try PracticeSession.DatabaseRepository.end(session, for: user, on: conn).wait()
 
+            let uri = try "/api/practice-sessions/\(session.requireID())/result"
+            let response = try app.sendRequest(to: uri, method: .GET, headers: standardHeaders, loggedInUser: user)
+            response.has(statusCode: .ok)
+        }
     }
     
     static let allTests = [
-        ("testToAssignTask", testToAssignTask)
+        ("testResultsWithNoAnswers", testResultsWithNoAnswers)
     ]
 }
