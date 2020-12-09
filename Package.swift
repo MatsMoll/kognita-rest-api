ce2af6ddbce783330f560770f796373e538ba44f
Mats Eikeland Mollestad <mem@mollestad.no>
2020-12-09 18:09:26 +0100

    [fix] Markdown Tests

--- Package.swift
+++ Package.swift
@@ -31,6 +31,17 @@ case "DEV":
         ]
     )
 default:
+    #if os(macOS)
+    if ProcessInfo.processInfo.environment["CUSTOM_SETTINGS"] == nil {
+        dependencies.append(contentsOf: [
+                .package(path: "../KognitaCore"),
+                .package(path: "../KognitaModels"),
+                .package(path: "../../QTIKit")
+            ]
+        )
+        break
+    }
+    #endif
     dependencies.append(contentsOf: [
             .package(name: "KognitaCore", url: "https://Kognita:dyjdov-bupgev-goffY8@github.com/MatsMoll/KognitaCore", from: "2.0.0"),
             .package(name: "KognitaModels", url: "https://Kognita:dyjdov-bupgev-goffY8@github.com/MatsMoll/KognitaModels", from: "1.0.0"),
